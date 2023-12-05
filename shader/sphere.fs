7c9df669fd959b6ee35a73b7ce983c504b35dd1b
kxkxzzz <2574810576@qq.com>
2023-12-06 00:53:38 +0800

    refract

--- shader/sphere.fs
+++ shader/sphere.fs
@@ -7,9 +7,32 @@ in vec3 Position;
 uniform vec3 cameraPos;
 uniform samplerCube skybox;
 
+vec3 culReflectLight();
+vec3 culRefractLight();
+
 void main()
 {             
+    vec3 reflectColor = culReflectLight();
+    vec3 refractColor = culRefractLight();
+    float alpha = 0.8;
+    
+    FragColor = vec4(mix(reflectColor, refractColor, alpha), alpha);
+    //FragColor = vec4(refractColor,1.0f);
+}
+
+vec3 culReflectLight()
+{
     vec3 I = normalize(Position - cameraPos);
     vec3 R = reflect(I, normalize(Normal));
-    FragColor = vec4(texture(skybox, R).rgb, 1.0);
+    return vec3(texture(skybox, R).rgb);
+}
+
+vec3 culRefractLight()
+{
+    vec3 N = normalize(Normal);
+    vec3 I = normalize(Position - cameraPos);
+    vec3 T = refract(I, N, 0.66);
+
+    vec4 refractedColor = texture(skybox, T);
+    return vec3(refractedColor.rgb);
 }
